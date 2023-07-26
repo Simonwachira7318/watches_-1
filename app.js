18c0bd48b4612e84325db8e78432d17f9b403a20
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-07-26 13:52:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// redirection of social media accounts
+function redirectToFacebook() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100071584461977";
+  }
+  // redirection of social media accounts
+function redirectToInstagram() {
+    window.location.href = "https://instagram.com/its__wachira?igshid=NGExMmI2YTkyZg==";
+  }
+  
+  // redirection of social media accounts
+  function redirectToTwitter() {
+    window.location.href = "https://twitter.com/Its_wachira2?t=P5t0BhEmEFgfYGYECE7OdQ&s=35";
+  }
+  
+  // redirection of social media accounts
+  function redirectToLinkedin() {
+    window.location.href = "https://www.linkedin.com/in/simon-wachira-680b88268";
+  }
+  
+  // Toggle menu functionality
+  function toggleMenu() {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.classList.toggle('active');
+  }
+  
+      // JavaScript function to scroll the reviews
+      // function scrollReviews(direction) {
+      //   const reviewClass = document.querySelector('.review-class');
+      //   const reviewWidth = reviewClass.offsetWidth;
+      //   const scrollAmount = 200;
+  
+      //   if (direction === 'left') {
+      //     reviewClass.scrollLeft -= scrollAmount;
+      //   } else if (direction === 'right') {
+      //     reviewClass.scrollLeft += scrollAmount;
+      //   }
+      // }
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+
+// const navbarLinkks = document.getElementsByClassName('navbar-links')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinkks.classList.toggle('active')
+// })
